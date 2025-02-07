47f8065a62efe39adc0a3c68e732328a9dd390b3
Debojyoti Ghosh <debojyoti.ghosh@gmail.com>
2025-02-06 17:33:20 -0800

    added clang-format test
    More fixes to the clang-format CI test

---
+++ .github/workflows/tests/test_clang-format.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+root_dir=$PWD
+src_dir=$PWD/src
+
+format_script="$root_dir/utilities/custom-clang-format.py"
+
+n_fail=0
+
+echo "Checking header files."
+for f in $src_dir/*.H; do
+    echo "    $f"
+    python3 $format_script < $f > $f.tmp
+    diff_output=$(diff $f $f.tmp)
+    if [[ ! -z "$diff_output" ]]; then
+        ((n_fail+=1))
+        echo "$f does not follow the ExaEpi style guide."
+        echo "--"
+        echo "Output of diff $f $f.tmp:"
+        diff $f $f.tmp
+        echo "--"
+        echo ""
+    fi
+    rm $f.tmp
+done
+
+echo "Checking source files."
+for f in $src_dir/*.cpp; do
+    echo "    $f"
+    python3 $format_script < $f > $f.tmp
+    diff_output=$(diff $f $f.tmp)
+    if [[ ! -z "$diff_output" ]]; then
+        ((n_fail+=1))
+        echo "$f idoes not follow the ExaEpi style guide"
+        echo "--"
+        echo "Output of diff $f $f.tmp:"
+        diff $f $f.tmp
+        echo "--"
+        echo ""
+    fi
+    rm $f.tmp
+done
+
+if [[ $n_fail -gt 0 ]]; then
+  echo "$n_fail files failed style check."
+  exit 1
+fi
