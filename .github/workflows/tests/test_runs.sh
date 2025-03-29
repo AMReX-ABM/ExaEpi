ea73a1627caa31a1e5ba5f6ad671f96abf747373
Debojyoti Ghosh <debojyoti.ghosh@gmail.com>
2025-03-28 17:06:06 -0700

    corrections to testing script and adding nproc & nomp as arguments

--- .github/workflows/tests/test_runs.sh
+++ .github/workflows/tests/test_runs.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+nproc=$1 # number of MPI ranks
+nomp=$2  # number of OpenMP threads
+
 root_dir=$PWD
 tests_dir=$root_dir/tests
 data_dir=$root_dir/data
 bin_dir=$root_dir/build/bin
-tmp_dir=$root_dir/../tmp
+tmp_dir=$tests_dir/tmp
 
 exaepi_exec=$(ls $bin_dir/agent)
 if [ -z "$exaepi_exec" ]; then
@@ -13,31 +16,33 @@ if [ -z "$exaepi_exec" ]; then
 fi
 
 # create tmp dir
+echo "Creating $tmp_dir"
 if [ -d "tmp_dir" ]; then
     rm -rf $tmp_dir
 fi
 mkdir $tmp_dir
-cd $tmp_dir
 
 set -e
 
+cd $tests_dir
 # create run directories and run tests
-for i in $tests_dir/inputs*; do
+for i in inputs*; do
     dirname=run.$i
-    echo "Creating run directory $dirname"
-    mkdir $dirname
+    echo "Creating run directory $dirname in $tmp_dir"
+    mkdir $tmp_dir/$dirname
     echo "  Entering $dirname"
-    cd $dirname
+    pushd $tmp_dir/$dirname
 #   copy input file
     cp $tests_dir/$i .
 #   make symlinks to data files
     ln -sf $data_dir .
 #   run ExaEpi
     echo "  Running $exaepi_exec with inputs $i..."
-    mpiexec -n 4 $exaepi_exec $i
+    export OMP_NUM_THREADS=$nomp
+    mpiexec -n $nproc $exaepi_exec $i
 #   done
     echo "  Done."
-    cd ..
+    popd
     echo  ""
 done
 
