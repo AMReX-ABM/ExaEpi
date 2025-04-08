3d67ccd15abc7c4ba08b7173166bb19c5dcf2be0
Tan Nguyen <tannguyen@lbl.gov>
2025-04-08 12:19:43 -0700

    fix the freezing bug in case initialization

--- src/InitializeInfections.cpp
+++ src/InitializeInfections.cpp
@@ -50,7 +50,7 @@ static int infectRandomCommunity (AgentContainer& pc,                      /*!< 
     const auto domain = geom.Domain();
 
     int num_infected = 0;
-    for (MFIter mfi = pc.MakeMFIter(0); mfi.isValid(); ++mfi) {
+    for (MFIter mfi(comm_mf); mfi.isValid(); ++mfi) {
         DenseBins<AgentContainer::ParticleType>& bins = bin_map[std::make_pair(mfi.index(), mfi.LocalTileIndex())];
         auto& agents_tile = pc.GetParticles(0)[std::make_pair(mfi.index(), mfi.LocalTileIndex())];
         auto& aos = agents_tile.GetArrayOfStructs();
