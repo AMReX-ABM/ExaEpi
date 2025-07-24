e46504261c2024da929c0cf36897c8964658e803
Tan Nguyen <nnguyent@aurora-uan-0010.hostmgmt.cm.aurora.alcf.anl.gov>
2025-07-24 01:42:43 +0000

    restrict the changes to SYCL only

--- src/InitializeInfections.cpp
+++ src/InitializeInfections.cpp
@@ -28,7 +28,7 @@ typedef std::map<std::pair<int, int>, DenseBins<AgentContainer::ParticleType>> B
     + Sum up number of infected agents over all processors and return that value.
 */
 static int infectRandomCommunity (AgentContainer& pc,                      /*!< Agent container (particle container)*/
-#ifdef AMREX_USE_GPU
+#ifdef AMREX_USE_SYCL
                                   AgentContainer& pc_h,                    /*!< A copy of Agent container on the host*/
 #endif
                                   const Vector<int>& unit_community_start, /*!< Start community number for each unit */
@@ -55,7 +55,7 @@ static int infectRandomCommunity (AgentContainer& pc,                      /*!< 
     int num_infected = 0;
     for (MFIter mfi = pc.MakeMFIter(0); mfi.isValid(); ++mfi) {
         DenseBins<AgentContainer::ParticleType>& bins = bin_map[std::make_pair(mfi.index(), mfi.LocalTileIndex())];
-#ifdef AMREX_USE_GPU
+#ifdef AMREX_USE_SYCL
         auto& agents_tile = pc_h.GetParticles(0)[std::make_pair(mfi.index(), mfi.LocalTileIndex())];
         if (fast_bin) { agents_tile = pc.GetParticles(0)[std::make_pair(mfi.index(), mfi.LocalTileIndex())]; }
 #else
@@ -166,9 +166,9 @@ void setInitialCasesFromFile (AgentContainer& pc,                      /*!< Agen
     BL_PROFILE("setInitialCasesFromFile");
 
     std::map<std::pair<int, int>, amrex::DenseBins<AgentContainer::ParticleType>> bin_map;
-#ifdef AMREX_USE_GPU
+#ifdef AMREX_USE_SYCL
     AgentContainer pc_h;
-    dtoh_memcpy(&pc_h, &pc, sizeof(ParticleContainer));
+    dtoh_memcpy((void*)&pc_h, (void*)&pc, sizeof(ParticleContainer));
 #endif
 
     Print() << "Initializing infections for " << d_name << "\n";
@@ -192,10 +192,10 @@ void setInitialCasesFromFile (AgentContainer& pc,                      /*!< Agen
                     ntry = diff > 5 ? 5 : diff;
                     int nSuccesses =
                             infectRandomCommunity(pc,
-#ifdef AMREX_USE_GPU
-                            pc_h,
+#ifdef AMREX_USE_SYCL
+                                                  pc_h,
 #endif
-                            unit_community_start, comm_mf, bin_map, units[u], d_idx, ntry, fast_bin);
+                                                  unit_community_start, comm_mf, bin_map, units[u], d_idx, ntry, fast_bin);
                     ninf += nSuccesses;
                     i += nSuccesses;
                     u = (u + 1) % units.size(); // sometimes we infect fewer than ntry, but switch to next unit anyway
@@ -216,9 +216,9 @@ void setInitialCasesRandom (AgentContainer& pc,                      /*!< Agent 
     BL_PROFILE("setInitialCasesRandom");
 
     std::map<std::pair<int, int>, amrex::DenseBins<AgentContainer::ParticleType>> bin_map;
-#ifdef AMREX_USE_GPU
+#ifdef AMREX_USE_SYCL
     AgentContainer pc_h;
-    dtoh_memcpy(&pc_h, &pc, sizeof(ParticleContainer));
+    dtoh_memcpy((void*)&pc_h, (void*)&pc, sizeof(ParticleContainer));
 #endif
 
     Print() << "Initializing infections for " << d_name << "\n";
@@ -231,10 +231,10 @@ void setInitialCasesRandom (AgentContainer& pc,                      /*!< Agent 
             if (ParallelDescriptor::IOProcessor()) { unit = Random_int(unit_community_start.size() - 1); }
             ParallelDescriptor::Bcast(&unit, 1);
             int nSuccesses = infectRandomCommunity(pc,
-#ifdef AMREX_USE_GPU
-                pc_h,
+#ifdef AMREX_USE_SYCL
+                                                   pc_h,
 #endif
-                unit_community_start, comm_mf, bin_map, unit, d_idx, 1, fast_bin);
+                                                   unit_community_start, comm_mf, bin_map, unit, d_idx, 1, fast_bin);
             ninf += nSuccesses;
             i += nSuccesses;
         }
