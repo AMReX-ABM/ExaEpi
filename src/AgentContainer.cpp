b7de5dde049aeb62c1b9d7153f3d38410813fdc2
Andrew Myers <atmyers2@gmail.com>
2025-12-16 09:22:04 -0800

    a few more fixes

--- src/AgentContainer.cpp
+++ src/AgentContainer.cpp
@@ -751,6 +751,7 @@ std::array<Long, OutputStatus::nattribs> AgentContainer::getTotals (const int a_
     BL_PROFILE("getTotals");
     static_assert(OutputStatus::nattribs == 20, "Expected nattribs == 20");
 
+    const auto* disease_parm_d = getDiseaseParameters_d(a_d);
     amrex::ReduceOps<REPEAT(18, ReduceOpSum)> reduce_ops;
     auto r = amrex::ParticleReduce<ReduceData<REPEAT(20, int)>>(
             *this,
