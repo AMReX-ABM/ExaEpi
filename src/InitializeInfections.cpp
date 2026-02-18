d48ea7a365f65c3810d68ed77acf1e9da2be59a4
stevenhofmeyr <shofmeyr@lbl.gov>
2026-02-18 08:55:22 -1000

    Optimize upop io (#144)
    * Write out urbanpop data in binary and read it in binary
    
    * Fixed types
    
    * Updated NM datasets for binary format
    
    * Added an early exit if there are no more infected agents
    
    * updated test to use binary file for NM UrbanPop
    
    * Added a version of the UrbanPop conversion script that uses Polars instead of Pandas and is hence much faster
    
    * Added binary printing of agents to polars UrbanPop script.
    
    * Improved efficiency and replicability for UrbanPop conversion script
    
    * More attempts to make UrbanPop polars generation script deterministic
    
    * Improved performance of teacher allocations for polars UrbanPop script
    
    * Fixed bug with zero padding
    
    * removed unecessary sort and added output of cpp header file
    
    * Set seed in polars sample for reproducibility
    
    * Optimize childcare selection
    
    * Using lazy scanning for improved performance when reading feather files
    
    * optimized writing of index
    
    * Added printing of index in binary format
    
    * Added reading of binary index in ExaEpi
    
    * Changed UrbanPop script to write a single binary file that includes the index, and read it from ExaEpi.
    
    * Added education schools data file
    
    * gzipped schools data file
    
    * deleted unzipped school file
    
    * Updated urbanpop NM file for combined binary format
    
    * Removed old urbanpop idx file
    
    * Fixed bug where whole UrbanPop binary file was read into memory and broadcasted
    
    * Don't reopen UrbanPop file
    
    * updated test file for new urbanpop file naming convention

--- src/InitializeInfections.cpp
+++ src/InitializeInfections.cpp
@@ -160,7 +160,14 @@ void setInitialCasesFromFile (AgentContainer& pc,                      /*!< Agen
     std::map<std::pair<int, int>, amrex::DenseBins<AgentContainer::ParticleType>> bin_map;
 
     Print() << "Initializing infections for " << d_name << "\n";
+#ifdef COMPARE_TO_EPICAST
+    Print() << "WARNINNG: limited version for comparing to Epicast\n";
+    const int NTRY = 1;
+#warning Building limited version for comparing to Epicast
+#else
     const int NTRY = 10;
+#endif
+
     int ntry = NTRY;
     int ninf = 0;
     for (int ihub = 0; ihub < cases.N_hubs; ++ihub) {
--- src/main.cpp
+++ src/main.cpp
@@ -241,6 +241,24 @@ void runAgent () {
                 Abort("Unimplemented ic_type");
             }
 
+#ifdef AMREX_DEBUG
+            //  dump a text file of the initial agent fields for debugging purposes
+            string agents_fname =
+                    std::string("initial_agents.") + (params.ic_type == ICType::UrbanPop ? "urbanpop" : "census") + ".csv";
+            pc.WriteAsciiFile(agents_fname);
+            if (ParallelDescriptor::IOProcessor()) {
+                std::ofstream agents_f(agents_fname, std::ios_base::app);
+                agents_f << "#posx posy id cpu " << "treatment_timer " << "disease_counter " << "prob " << "latent_period "
+                         << "infectious_period " << "incubation_period " << "hospital_delay " << "age_group " << "family "
+                         << "home_i "
+                         << "home_j " << "work_i " << "work_j " << "hosp_i " << "hosp_j " << "trav_i " << "trav_j " << "nborhood "
+                         << "hh_cluster " << "school_grade "
+                         << "school_id " << "school_closed " << "naics " << "workgroup " << "work_nborhood " << "withdrawn "
+                         << "random_travel " << "air_travel " << "status " << "symptomatic\n";
+                agents_f.close();
+            }
+#endif
+
             for (int d = 0; d < params.num_diseases; d++) {
                 auto disease_params = pc.getDiseaseParameters_h(d);
                 if (disease_params->initial_case_type == CaseTypes::file) {
@@ -321,29 +339,14 @@ void runAgent () {
         }
     }
 
-#ifdef AMREX_DEBUG
-    //  dump a text file of the initial agent fields for debugging purposes
-    string agents_fname = std::string("initial_agents.") + (params.ic_type == ICType::UrbanPop ? "urbanpop" : "census") + ".csv";
-    pc.WriteAsciiFile(agents_fname);
-    if (ParallelDescriptor::IOProcessor()) {
-        std::ofstream agents_f(agents_fname, std::ios_base::app);
-        agents_f << "#posx posy id cpu " << "treatment_timer " << "disease_counter " << "prob " << "latent_period "
-                 << "infectious_period " << "incubation_period " << "hospital_delay " << "age_group " << "family " << "home_i "
-                 << "home_j " << "work_i " << "work_j " << "hosp_i " << "hosp_j " << "trav_i " << "trav_j " << "nborhood "
-                 << "hh_cluster " << "school_grade "
-                 << "school_id " << "school_closed " << "naics " << "workgroup " << "work_nborhood " << "withdrawn "
-                 << "random_travel " << "air_travel " << "status " << "symptomatic\n";
-        agents_f.close();
-    }
-#endif
-
     std::vector<int> step_of_peak(params.num_diseases, 0);
     std::vector<Long> num_infected_peak(params.num_diseases, 0);
     std::vector<Long> cumulative_deaths(params.num_diseases, 0);
     for (int d = 0; d < params.num_diseases; d++) {
         auto counts = pc.getTotals(d);
-        if (totalInfected(counts) > num_infected_peak[d]) {
-            num_infected_peak[d] = totalInfected(counts);
+        auto total_infected = totalInfected(counts);
+        if (total_infected > num_infected_peak[d]) {
+            num_infected_peak[d] = total_infected;
             step_of_peak[d] = 0;
         }
         cumulative_deaths[d] = counts[OutputStatus::D];
@@ -506,11 +509,13 @@ void runAgent () {
                 pc.moveAirTravel(censusData.unit_mf, air, censusData.demo);
             }
 
-            // Typical day
+// Typical day
+#ifndef COMPARE_TO_EPICAST
             pc.morningCommute(mask_behavior);
             pc.interactDay(mask_behavior);
             pc.eveningCommute(mask_behavior);
             pc.interactEvening(mask_behavior);
+#endif
             pc.interactNight(mask_behavior);
 
             if ((params.random_travel_int > 0) && (i % params.random_travel_int == 0)) { pc.returnRandomTravel(); }
@@ -532,6 +537,9 @@ void runAgent () {
             Print() << "; deaths: " << cumulative_deaths[0] << "\n";
 
             cur_time += 1.0_rt; // time step is one day
+
+            // early exit if no more spreading or deaths can occur
+            if (num_infected[0] == 0) { break; }
         }
     }
 
