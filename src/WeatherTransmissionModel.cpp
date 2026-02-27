03a61c34fbbbed48243752b78be386d6d0700938
Andrew Myers <atmyers2@gmail.com>
2026-02-27 14:49:36 -0800

    fix formatting

--- src/WeatherTransmissionModel.cpp
+++ src/WeatherTransmissionModel.cpp
@@ -7,11 +7,10 @@
 #include <AMReX_ParmParse.H>
 
 /*! \brief Read weather-transmission model parameters from the inputs file. */
-void WeatherTransmissionModel::readInputs (const std::string& pp_str)
-{
+void WeatherTransmissionModel::readInputs (const std::string& pp_str) {
     amrex::ParmParse pp(pp_str);
-    pp.query("p_max",   p_max);
+    pp.query("p_max", p_max);
     pp.query("beta_AH", beta_AH);
-    pp.query("T0",      T0);
+    pp.query("T0", T0);
     pp.query("alpha_T", alpha_T);
 }
