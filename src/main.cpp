f7af9a53505a7cceb285d573c694747b7f57123f
Steven Hofmeyr <shofmeyr@lbl.gov>
2026-04-04 12:34:37 -1000

    disable epicast comparison by default

--- src/main.cpp
+++ src/main.cpp
@@ -544,12 +544,10 @@ void runAgent () {
             }
 
             // Typical day
-            // #ifndef COMPARE_TO_EPICAST
             pc.morningCommute(mask_behavior);
             pc.interactDay(mask_behavior);
             pc.eveningCommute(mask_behavior);
             pc.interactEvening(mask_behavior);
-            // #endif
             pc.interactNight(mask_behavior);
 
             if ((params.random_travel_int > 0) && (i % params.random_travel_int == 0)) { pc.returnRandomTravel(); }
